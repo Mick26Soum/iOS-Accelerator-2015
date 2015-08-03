de6c20194d72988ca002806d4a45c67d1aa26f54
Mick Soum <mick26soum@gmail.com>
2015-08-03 10:36:28 -0700

    first commit

---
+++ Twitter Clone/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  Twitter Clone
+//
+//  Created by Mick Soumphonphakdy on 8/3/15.
+//  Copyright (c) 2015 Mick Soum. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+   
+}
